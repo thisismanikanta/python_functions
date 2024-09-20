37107e8af18dccf7f0bfe651121698c34bc8cc2e
G M ANJANEYA <gantamanikanta46@gmail.com>
2024-09-20 12:14:02 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+#write functions for basic arithmetic operations.create a program thet takes two numbers as input and return the result.
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+def add(a,b):
+   return a+b
+def sub(a,b):
+   return a-b
+def mul(a,b):
+   return a*b
+def div(a,b):
+   return a/b
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+
+
+#output:
+enter a:4
+enter b:2
+6
+2
+8
+2.0
